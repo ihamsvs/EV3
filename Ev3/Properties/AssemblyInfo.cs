795d8f9307f433b6be6fb359541cb30b0d940b7f
ihamsvs <68127084+ihamsvs@users.noreply.github.com>
2022-12-27 01:40:33 -0300

    Algunas vistas ya creadas, se necesita solo crear la bd y hacer lo que falta

---
+++ Ev3/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general sobre un ensamblado se controla mediante lo siguiente
+// conjunto de atributos. Cambie los valores de estos atributos para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("Ev3")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Técnicos Inside Lab")]
+[assembly: AssemblyProduct("Ev3")]
+[assembly: AssemblyCopyright("Copyright © Técnicos Inside Lab 2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si configura ComVisible como falso, los tipos de este ensamblado no se hacen visibles
+// para componentes COM.  Si necesita acceder a un tipo de este ensamblado desde
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID es para el ID. typelib cuando este proyecto esté expuesto a COM
+[assembly: Guid("7a486e49-c8b8-4e87-ba6d-f9957cbc17f8")]
+
+// La información de versión de un ensamblado consta de los siguientes cuatro valores:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o puede predeterminar los números de compilación y de revisión
+// mediante el carácter '*', como se muestra a continuación:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
